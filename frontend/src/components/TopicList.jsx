7e9b12f755f33807bf99ea5e5ae7021f3124b811
carolineolagunju <olamidejr21@gmail.com>
2023-09-17 00:43:39 -0600

    add logic to loop through props

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+// const sampleDataForTopicList = [
+//   {
+//     id: "1",
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: "2",
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: "3",
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
-const TopicList = () => {
-  return (
-    <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
-    </div>
-  );
+const TopicList = (props) => {
+  const topicData = props.topics.map((topic) => {
+    return (
+      <TopicListItem slug={topic.slug} title={topic.title} key={topic.id} />
+    );
+  });
+
+  return <div className="top-nav-bar__topic-list">{topicData}</div>;
 };
 
 export default TopicList;
