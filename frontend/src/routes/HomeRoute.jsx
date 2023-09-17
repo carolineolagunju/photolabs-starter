a99b730fcdf64c11377c8b4eb66ca2e6bc6d758b
carolineolagunju <olamidejr21@gmail.com>
2023-09-17 00:48:55 -0600

    implement function isPhotoExist, handlePhotoClick and logic to return TopNaigation and PhotoList Component

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import PhotoList from "components/PhotoList";
+import TopNavigation from "components/TopNavigationBar";
+import "../styles/HomeRoute.scss";
 
-import '../styles/HomeRoute.scss';
+const HomeRoute = ({ photos, topics, photoClick, isFavorite, addFavorite, favorites, handleAddFav }) => {
+ 
+  //return true if favorites array is not empty
+  const isFavPhotoExist = (favorites) => favorites.length > 0;
 
-const HomeRoute = () => {
+  const handlePhotoClick = (photo, id, name, profile, city, country) => {
+    photoClick(photo, id, name, profile, city, country);
+  };
+//return TopNavigation to render
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation
+        topics={topics}
+        isFavPhotoExist={isFavPhotoExist(favorites)}
+      />
+      <PhotoList
+        photos={photos}
+        addFavorite={addFavorite}
+        isFavorite={isFavorite}
+        photoClick={handlePhotoClick}
+        onClick={handleAddFav}
+      />
     </div>
   );
 };
