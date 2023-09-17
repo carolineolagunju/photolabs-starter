657bfd5c30cedc4591d3363488908cdfce7e9165
carolineolagunju <olamidejr21@gmail.com>
2023-09-17 00:38:43 -0600

    define onClick && add props to component FavIcon

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,14 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+  
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={props.onClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        {<FavIcon selected={props.selected} />}
       </div>
     </div>
   );
