dcaad4494d9958361b9ff22bc9dfced32e8b1483
carolineolagunju <olamidejr21@gmail.com>
2023-09-17 00:34:10 -0600

    implement function isFavorite, handleAddFav, photoClick, clseModal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
-
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+import React, { useState } from "react";
+import "./App.scss";
+import HomeRoute from "routes/HomeRoute";
+import photos from "./mocks/photos";
+import topics from "./mocks/topics";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
+import PhotoList from "components/PhotoList";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  //set state for modal to false as its default state
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [displayPicture, setDisplayPicture] = useState(null);
+
+  const [favorites, setFavorites] = useState([]);
+  const addFavorite = (id) => {
+    //if id in arr, remove, else add
+    if (favorites.includes(id)) {
+      return setFavorites(favorites.filter((photoId) => photoId !== id));
+    }
+    //add the photo id to the array
+    setFavorites([...favorites, id]);
+  };
+
+  const isFavorite = (id) => {
+    //return true if id is in the array
+    return favorites.includes(id);
+  };
+
+  const handleAddFav = (id) => {
+    addFavorite(id);
+  };
+
+  //function to open modal when photo is clicked
+  const photoClick = (photo, id, name, profile, city, country) => {
+    setIsModalOpen(true);
+    setDisplayPicture({photo, id, name, profile, city, country});
+
+  };
+
+  
+  //function to close modal when x is clicked
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+ 
   return (
     <div className="App">
-      <PhotoListItem/>
+      <HomeRoute photos={photos} topics={topics} photoClick={photoClick} isFavorite={isFavorite} addFavorite={addFavorite} favorites={favorites} handleAddFav={handleAddFav}/>
+      {isModalOpen && (
+        <PhotoDetailsModal closeModal={closeModal} photo={displayPicture} photoList={photos} isFavorite={isFavorite} addFavorite={addFavorite} onClick={handleAddFav}/>
+      )}
     </div>
   );
 };
