27ba3c893a5dd90f9929fc83cf2f1005364b9820
carolineolagunju <olamidejr21@gmail.com>
2023-09-18 14:26:36 -0600

    add comment

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import FavIcon from './FavIcon';
+import React from "react";
+import FavIcon from "./FavIcon";
 
-import '../styles/FavBadge.scss';
+import "../styles/FavBadge.scss";
 
 const FavBadge = ({ isFavPhotoExist }) => {
-
+  //selected is set to true to paint the heart icon on the navbar
   return (
-    <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist} selected={true}/>
+    <div className="fav-badge">
+      <FavIcon displayAlert={!!isFavPhotoExist} selected={true} />
     </div>
-  ) 
+  );
 };
 
 export default FavBadge;
