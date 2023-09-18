2bd3e25b7b3e8738b7f0fa277f7d2f3bebd7d361
carolineolagunju <olamidejr21@gmail.com>
2023-09-18 14:20:59 -0600

    implement useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,46 +1,86 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import photos from "../mocks/photos"
-const useApplicationData = () =>{
-    //set state for modal to false as its default state
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [photoSelected, setPhotoSelected] = useState(null);
-    const [favorites, setFavorites] = useState([]);
 
+
+//default states
+const defaultState = {
+  isModalOpen: false,
+  photoSelected: null,
+  favorites: [],
+}
+
+
+//actions
+const ACTIONS = {
+  CLOSE_MODAL: 'CLOSE_MODAL',
+  TOGGLE_FAVORITE: 'TOGGLE_FAVORITE',
+  ON_PHOTO_CLICK: 'ON_TOGGLE_FAVORITE'
+}
+
+
+//reducer function
+const reducer = (state, action) => {
+  //set isModalOpen default state to false
+  if (action.type === ACTIONS.CLOSE_MODAL) {
+    return {...state, isModalOpen: false};
+  }
+  //set isModalOpen default state to true && photoSelected default state
+  //to the photoObject with that id
+  if (action.type === ACTIONS.ON_PHOTO_CLICK) {
+    const selectedPhoto = photos.find(photo => photo.id === action.payload.id);
+    return {...state, isModalOpen: true, photoSelected: selectedPhoto}
+  }
+  //set favorites default state to the returned array
+  //this loigc is used to control the favIcon. when the favIcon is deselected, the logic removes the id of 
+  //that picture from the favorites array  else it adds it to the favorites array
+  if (action.type === ACTIONS.TOGGLE_FAVORITE) {
+    if(state.favorites.includes(action.payload.id)) {
+      return {...state, favorites: state.favorites.filter((photoId) => photoId !== action.payload.id)}
+    } else {
+      return  { ...state, favorites: [...state.favorites, action.payload.id] };
+    }
+  }
+  throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
+}
+
+
+const useApplicationData = () =>{
+
+  //using useReducer to set state
+  const [state, dispatch] = useReducer(reducer, defaultState)
 
     const toggleFavorite = (id) => {
+      dispatch({type: ACTIONS.TOGGLE_FAVORITE, payload: {id}})
       //if id in arr, remove, else add
-      if (favorites.includes(id)) {
-        return setFavorites(favorites.filter((photoId) => photoId !== id));
-      }
+      // if (state.favorites.includes(id)) {
+        // return setFavorites(favorites.filter((photoId) => photoId !== id));
+      // }
       //add the photo id to the array
-      setFavorites([...favorites, id]);
+      // setFavorites([...favorites, id]);
     };
   
   
     //function to open modal when photo is clicked
     const onPhotoClick = (id) => {
-      setIsModalOpen(true);
-      const selectedPhoto = photos.find(photo => photo.id === id);
-      setPhotoSelected(selectedPhoto);
+      dispatch({type: ACTIONS.ON_PHOTO_CLICK, payload: {id}})
+      // setIsModalOpen(true);
+      // const selectedPhoto = photos.find(photo => photo.id === id);
+      // setPhotoSelected(selectedPhoto);
    
     };
   
     
     //function to close modal when x is clicked
     const closeModal = () => {
-      setIsModalOpen(false);
+      dispatch({type: ACTIONS.CLOSE_MODAL })
+      // setIsModalOpen(false);
     };
 
     return {
-      isModalOpen,
-      setIsModalOpen,
-      photoSelected,
-      setPhotoSelected,
-      favorites,
-      setFavorites,
+      state,
       toggleFavorite,
       onPhotoClick,
-      closeModal,
+      closeModal
     }
 }
 
