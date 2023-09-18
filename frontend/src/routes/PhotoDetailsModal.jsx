b8b1023f7443377f56fc48ed78179a786759c5f1
carolineolagunju <olamidejr21@gmail.com>
2023-09-18 14:39:57 -0600

    add comment, remove dead code

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,58 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 import PhotoList from "components/PhotoList";
 import PhotoFavButton from "components/PhotoFavButton";
 
+//function that handles items to be displayed on the modal once a picture is clicked
 const PhotoDetailsModal = ({
   closeModal,
-  photo,
-  photoList,
-  addFavorite,
-  isFavorite,
-  onClick,
+  photoSelected,
+  toggleFavorite,
+  favorites,
 }) => {
-  const photosObj = photoList.find((pic) => pic.id === photo.id);
-  const similarPhotos = [];
-  if (photosObj) {
-    for (const obj in photosObj.similar_photos) {
-      similarPhotos.push(photosObj.similar_photos[obj]);
-    }
-  }
-
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" onClick={closeModal} />
       </button>
-      <PhotoFavButton selected={isFavorite} />
-      <img
-        className="photo-details-modal__images"
-        key={photo.id}
-        src={photo.photo}
-        alt="large image"
-      />
-      <div className="photo-list__user-details">
-        <img
-          src={photo.profile}
-          alt="profile picture"
-          className="photo-list__user-profile"
+      <div className="photo-details-modal__images">
+        <PhotoFavButton
+          toggleFavorite={toggleFavorite}
+          favorites={favorites}
+          id={photoSelected.id}
         />
-        <div className="photo-list__user-info">
-          {photo.name}
-          <div className="photo-list__user-location">
-            {photo.city}, {photo.country}
+        <img
+          className="photo-details-modal__image"
+          key={photoSelected.id}
+          src={photoSelected.urls.full}
+          alt="large image"
+        />
+
+        <div className="photo-details-modal__photographer-details ">
+          <img
+            className="photo-details-modal__photographer-profile"
+            src={photoSelected.user.profile}
+          />
+          <div className="photo-details-modal__photographer-info">
+            {photoSelected.user.name}
+            <div className="photo-details-modal__photographer-location">
+              {photoSelected.location.city},{photoSelected.location.country}
+            </div>
           </div>
         </div>
+
+
+        <h3 className="photo-details-modal__header">Similar Photos</h3>
+        <div className="photo-details-modal__images">
+          {
+            <PhotoList
+              photos={Object.values(photoSelected.similar_photos)}
+              favorites={favorites}
+              toggleFavorite={toggleFavorite}
+            />
+          }
+        </div>
       </div>
-    <h3 className="photo-details-modal__header">Similar Photos</h3>
-      <PhotoList
-        photos={similarPhotos}
-        addFavorite={addFavorite}
-        isFavorite={isFavorite}
-        onClick={onClick}
-      />
     </div>
   );
 };
