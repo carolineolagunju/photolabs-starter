d6b492bdecdc283fe5e3796ca9a2d4ac194f5fbf
carolineolagunju <olamidejr21@gmail.com>
2023-09-17 18:08:50 -0600

    implement logic to change selected to true on toggle

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,14 +1,17 @@
-import React, { useCallback, useState } from "react";
-
+import React from "react";
 import FavIcon from "./FavIcon";
 import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton(props) {
-  
+  const onToggleFavoriteClick = () => {
+    props.toggleFavorite(props.id);
+  }
+  //setting selected to true if the photo id exists in the favorites array
+  const selected = props.favorites.includes(props.id);
   return (
-    <div className="photo-list__fav-icon" onClick={props.onClick}>
+    <div className="photo-list__fav-icon" onClick={onToggleFavoriteClick}>
       <div className="photo-list__fav-icon-svg">
-        {<FavIcon selected={props.selected} />}
+        {<FavIcon selected={selected} />}
       </div>
     </div>
   );
