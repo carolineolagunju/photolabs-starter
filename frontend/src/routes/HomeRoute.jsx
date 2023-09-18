6d29b654b59f962dc015631034d171f5e6d82e10
carolineolagunju <olamidejr21@gmail.com>
2023-09-18 14:41:57 -0600

    add comment, fix indentation

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,29 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import PhotoList from "components/PhotoList";
 import TopNavigation from "components/TopNavigationBar";
 import "../styles/HomeRoute.scss";
 
-const HomeRoute = ({ photos, topics, photoClick, isFavorite, addFavorite, favorites, handleAddFav }) => {
- 
-  //return true if favorites array is not empty
-  const isFavPhotoExist = (favorites) => favorites.length > 0;
-
-  const handlePhotoClick = (photo, id, name, profile, city, country) => {
-    photoClick(photo, id, name, profile, city, country);
-  };
-//return TopNavigation to render
+const HomeRoute = ({
+  photos,
+  topics,
+  onPhotoClick,
+  toggleFavorite,
+  favorites,
+}) => {
+  //return TopNavigation to render
   return (
     <div className="home-route">
-      <TopNavigation
-        topics={topics}
-        isFavPhotoExist={isFavPhotoExist(favorites)}
-      />
+      <TopNavigation topics={topics} isFavPhotoExist={favorites.length > 0} />
       <PhotoList
         photos={photos}
-        addFavorite={addFavorite}
-        isFavorite={isFavorite}
-        photoClick={handlePhotoClick}
-        onClick={handleAddFav}
+        onPhotoClick={onPhotoClick}
+        favorites={favorites}
+        toggleFavorite={toggleFavorite}
       />
     </div>
   );
