1aeaae0ddf66918e4517b896b6e61e4bb27169ab
carolineolagunju <olamidejr21@gmail.com>
2023-09-19 16:44:47 -0600

    implememnt conditional rendering for when topics are selected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,25 @@ import HomeRoute from "routes/HomeRoute";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
 import useApplicationData from "hooks/useApplicationData";
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
-  const { state, toggleFavorite, onPhotoClick, closeModal } =
+  const { state, toggleFavorite, onPhotoClick, closeModal, getPhotosByTopic } =
     useApplicationData();
 
-  const { isModalOpen, photoSelected, favorites, photos, topics } = state;
+  const { isModalOpen, photoSelected, favorites, photos, topics, topicPhotos } =
+    state;
 
   return (
     <div className="App">
+      {/* renders photos conditionally */}
       <HomeRoute
-        photos={photos}
+        photos={topicPhotos.length > 0 ? topicPhotos : photos}
         topics={topics}
         onPhotoClick={onPhotoClick}
         favorites={favorites}
         toggleFavorite={toggleFavorite}
+        getPhotosByTopic={getPhotosByTopic}
       />
+
       {isModalOpen && (
         <PhotoDetailsModal
           closeModal={closeModal}
