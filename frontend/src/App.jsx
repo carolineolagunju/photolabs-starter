65dfed089121685c04bb9617ebf2a9d6217494aa
carolineolagunju <olamidejr21@gmail.com>
2023-09-19 00:48:12 -0600

    add useEffect to fetch topics data, update reducer function to to set topics state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,81 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
-//import photos from "./mocks/photos";
-import topics from "./mocks/topics";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
 import useApplicationData from "hooks/useApplicationData";
 
-
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  
-    const {
-      // isModalOpen,
-      // photoSelected,
-      // favorites,
-      state,
-      toggleFavorite,
-      onPhotoClick,
-      closeModal,
-    } = useApplicationData();
+  const { state, toggleFavorite, onPhotoClick, closeModal } =
+    useApplicationData();
 
+  const { isModalOpen, photoSelected, favorites, photos, topics } = state;
 
-  const {isModalOpen, photoSelected, favorites, photos} = state;
- 
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} onPhotoClick={onPhotoClick}  favorites={favorites} toggleFavorite={toggleFavorite}/>
+      <HomeRoute
+        photos={photos}
+        topics={topics}
+        onPhotoClick={onPhotoClick}
+        favorites={favorites}
+        toggleFavorite={toggleFavorite}
+      />
       {isModalOpen && (
-        <PhotoDetailsModal closeModal={closeModal} photoSelected={photoSelected}  toggleFavorite={toggleFavorite} favorites={favorites}/>
+        <PhotoDetailsModal
+          closeModal={closeModal}
+          photoSelected={photoSelected}
+          toggleFavorite={toggleFavorite}
+          favorites={favorites}
+        />
       )}
     </div>
   );
 };
 
 export default App;
-
-
-// import React from "react";
-// import "./App.scss";
-// import HomeRoute from "routes/HomeRoute";
-// import photos from "./mocks/photos";
-// import topics from "./mocks/topics";
-// import PhotoDetailsModal from "routes/PhotoDetailsModal";
-// import useApplicationData, { ACTIONS } from "hooks/useApplicationData";
-
-// const App = () => {
-//   const {
-//     state, // This includes isModalOpen, photoSelected, favorites
-//     toggleFavorite,
-//     onPhotoClick,
-//     closeModal,
-//     dispatch // This is the function to dispatch actions
-//   } = useApplicationData();
-
-//   const { isModalOpen, photoSelected, favorites } = state;
-
-//   return (
-//     <div className="App">
-//       <HomeRoute
-//         photos={photos}
-//         topics={topics}
-//         onPhotoClick={onPhotoClick}
-//         favorites={favorites}
-//         toggleFavorite={toggleFavorite}
-//       />
-//       {isModalOpen && (
-//         <PhotoDetailsModal
-//           closeModal={closeModal}
-//           photoSelected={photoSelected}
-//           toggleFavorite={(id) =>
-//             dispatch({ type: ACTIONS.TOGGLE_FAVORITE, payload: { id } })
-//           }
-//           favorites={favorites}
-//         />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default App;
